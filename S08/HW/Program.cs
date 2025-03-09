9a3541fafc73c99c1b0ba24aabac77ca7f3d3fbc
Mahsa-hajibande <mahsahajibande@gmail.com>
2025-03-09 11:44:25 -0700

    HW

---
+++ S08/HW/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW;
+
+public class Program
+{
+    
+    public static void String (string s , out string srev)
+    {
+        srev = "" ;
+        string [] MyString = s.Split (" ") ;
+        foreach (string Word in MyString)
+        {
+            srev = Word + " " + srev ;
+        }
+    }
+
+    public static void Reverse (string s , out string trev)
+    {
+        trev = "" ;
+        foreach (char letter in s)
+        {
+            trev = letter + trev ;
+        }
+    }
+
+    static void Main (string[] args)
+    {
+        String ("my name is Mahsa" , out string srev) ;
+        Console.WriteLine (srev) ;
+        Reverse ("my name is Mahsa" , out string trev) ;
+        Console.WriteLine (trev) ;
+    }
+}
+
