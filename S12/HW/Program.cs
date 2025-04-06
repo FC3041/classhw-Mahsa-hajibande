d4d7ea8c8a4372b117036b56645b3e8d93c2a7d8
Mahsa-hajibande <mahsahajibande@gmail.com>
2025-04-06 11:46:08 -0700

    HW

---
+++ S12/HW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using Transport;
+
+namespace HW;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ITransport Bike = new Bike (2 , 1) ;
+        ITransport Car = new Car (4 , 5) ;
+        ITransport Truck = new Truck (8 , 2) ;
+        Console.WriteLine($"Bike = Wheels: {Bike.WheelNum()}, Capacity: {Bike.Capacity()}");
+        Console.WriteLine($"Car = Wheels: {Car.WheelNum()}, Capacity: {Car.Capacity()}");
+        Console.WriteLine($"Truck = Wheels: {Truck.WheelNum()}, Capacity: {Truck.Capacity()}");
+    }
+}
